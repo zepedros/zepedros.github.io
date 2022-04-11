9d456dfb10a0e3b8f0c08d16841a7bbd45b3197e
Ze <zepedro4259@gmail.com>
2022-04-11 22:31:24 +0100

    added contact icons. no redirect yet

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,7 +1,9 @@
 import NewHeader from "../common/NewHeader"
 import NewTopMenu from "../common/NewTopMenu"
-export default function Contacts(){
-    return(
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
+import {MdEmail} from "react-icons/md"
+export default function Contacts() {
+    return (
         <div>
             <div className="hidden md:block">
                 <NewHeader></NewHeader>
@@ -9,12 +11,20 @@ export default function Contacts(){
             <div className="visible xl:block">
                 <NewTopMenu></NewTopMenu>
             </div>
-            
-            <h1 className='font-modenine'>CONTACTS
-                <br>
-                </br>
-                UNDER DEVELOPMENT 🛠️
-            </h1>
+
+            <p>
+                Here are my work contacts as well as important platforms
+                <br></br>
+                <br></br>
+                If you wish to contact me through my phone number, feel free to message me on LinkedIn or contact me via e-mail
+            </p>
+            <br></br>
+            <div className="centered-icons">
+                <AiFillLinkedin size={"5em"}></AiFillLinkedin>
+                <AiFillGithub size={"5em"}></AiFillGithub>
+                <MdEmail size={"5em"}></MdEmail>
+            </div>
+
         </div>
     )
 }
